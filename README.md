e8e88d0427598f1e60fc6ce598ee2aa658972628
shooding <wcting@loftechs.com>
2018-10-10 23:49:23 +0800

    check in codes and readme

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # twpoints
-計算台灣各種點數之間的最優交換比率(轉換路徑)
+
+計算台灣各種點數之間的最優交換比率(轉換路徑)。
+計算是否有任何迴圈會造成無限增長點數？
+
+# Install
+
+```bash
+brew install graphviz
+```
+
+# Usage
+
+```bash
+python points.py
+```
+
+# Reference
+
+- HamiPoint 點數交換專區 https://www.cheerspoint.com.tw/trans_point_index.php
+- LINE Points可以兌換成其他通路點數 http://official-blog.line.me/tw/archives/70132911.html
+- 華南 http://card.hncb.com.tw/wps/portal/card/area2/bonus/Points
+- HappyGo https://www.happygocard.com.tw/official/exchangearea/list.do
+- 亞洲萬里通 https://www.asiamiles.com/zh/earn-miles/retail/detail.retail.ap.taiwan.html
+- 亞洲萬里通 https://redeem.asiamiles.com/zh/gift-cards-and-miles-conversion/miles-conversion.html
+- OpenPoint http://www.7-11.com.tw/openpoint_event1/15_exchange/index.html
+- OpenPoint https://www.7-11.com.tw/openpoint_event1/15_exchange/airindex.html
+- PCHome https://24h.pchome.com.tw/point-ex/p2o
+- UUPoint https://uupon.tw/changePoint.do
+- Ponta https://www.ponta.com.tw/ExchArea.action
+- 中國信託 https://www.ctbcbank.com/html/long/creditcard/CNB2017010407/index.html
+- 聯邦 https://card.ubot.com.tw/eCard/reward.aspx
+- 歐付寶 https://www.opay.tw/Announcement/DetailAnnouncement?nID=1977
+- 玉山 https://www.esunbank.com.tw/bank/personal/credit-card/reward/rebates/pointexchange
+- 台新 https://www.taishinbank.com.tw/TS/TS02/TS0201/TS020104/TS02010404/
+- 花旗 https://www.citibank.com.tw/global_docs/chi/cc/gr-mileage/index.html
---
+++ points.py
@@ -0,0 +1,159 @@
+#coding=utf-8
+import networkx as nx
+import matplotlib.pyplot as plt
+import os, sys
+
+# Create Directed Graph
+G=nx.DiGraph()
+
+G.add_nodes_from(["HappyGo","OpenPoint","UUPoint","Ponta","台新","玉山","花旗紅利","花旗現金回饋","花旗哩程","華南",
+	"歐付寶","HamiPoint","亞洲萬里通","聯邦銀行","中國信託","LINE","PCHome","長榮","華航",
+	"JAL","ANA","KrisFlyer","彰銀","台北富邦"])
+
+edges = [
+	("HappyGo","LINE",float(30)/200),
+	("HappyGo","亞洲萬里通",float(100)/250),
+	("HappyGo","長榮",float(125)/250),
+	("HappyGo","PCHome",float(30)/150),	
+	("OpenPoint","HamiPoint",float(25)/7500),
+	("OpenPoint","PCHome",float(20)/6000),
+	("OpenPoint","亞洲萬里通",float(100)/20000),	
+	("OpenPoint","華航",float(100)/20000),	
+	("OpenPoint","ANA",float(200)/53000),
+	("OpenPoint","長榮",float(200)/45000),
+	("華航","OpenPoint",float(90000)/3500),
+	("ANA","OpenPoint",float(800000)/10000),
+	("ANA","UUPoint",float(7200)/10000),
+	("JAL","UUPoint",float(2000)/3000),
+	("JAL","Ponta",float(14000)/3000),
+	("長榮","OpenPoint",float(60000)/2000),
+	("UUPoint","亞洲萬里通",float(100)/250),
+	("UUPoint","JAL",float(125)/500),
+	("UUPoint","ANA",float(220)/1000),
+	("UUPoint","長榮",float(500)/1200),
+	("UUPoint","樂天",float(20)/100),
+	("UUPoint","歐付寶",float(25)/100),
+	("UUPoint","PCHome",float(25)/100),
+	("UUPoint","HamiPoint",float(6)/20),
+	("UUPoint","LINE",float(25)/110),
+	("Ponta","LINE",float(25)/750),
+	("Ponta","JAL",float(50)/1000),
+	("Ponta","長榮",float(250)/5000),
+	("Ponta","歐付寶",float(10)/250),
+	("Ponta","HamiPoint",float(30)/750),
+	("Ponta","中油",float(400)/100),
+	("台新","Ponta",float(1200)/1000),
+	("台新","華航",float(10000)/50000),
+	("台新","長榮",float(10000)/50000),
+	("台新","亞洲萬里通",float(4500)/1000),
+	("台新","UUPoint",float(15)/100),
+	("台新","OpenPoint",float(8500)/500),
+	("玉山","Ponta",float(650)/500),
+	("玉山","UUPoint",float(100)/500),
+	("玉山","OpenPoint",float(9000)/500),
+	("玉山","HappyGo",float(80)/500),
+	("玉山","華航",float(1250)/5000),
+	("玉山","長榮",float(1500)/5000),
+	("玉山","亞洲萬里通",float(2000)/5000),	
+	("花旗紅利","亞洲萬里通",float(2000)/6000),
+	("花旗紅利","華航",float(2000)/6000),
+	("花旗紅利","長榮",float(2000)/6000),
+	("花旗紅利","KrisFlyer",float(1000)/3000),
+	("花旗紅利","ThaiRoyal",float(1000)/3000),
+	("花旗紅利","HappyGo",float(300)/2200),
+	("花旗紅利","OpenPoint",float(10000)/700),
+	("花旗紅利","Ponta",float(2400)/2000),
+	("花旗現金回饋","HappyGo",float(150)/300),
+	("花旗現金回饋","Ponta",float(2500)/110),
+	("花旗現金回饋","華航",float(1000)/500),	
+	("花旗哩程","HappyGo",float(300)/900),
+	("花旗哩程","Ponta",float(2800)/1000),
+	("花旗哩程","亞洲萬里通",float(500)/500),
+	("花旗哩程","華航",float(500)/500),
+	("花旗哩程","長榮",float(500)/500),
+	("花旗哩程","KrisFlyer",float(500)/500),
+	("花旗哩程","ThaiRoyal",float(500)/500),
+	("花旗哩程","Etihad",float(500)/500),
+	("花旗哩程","Qatar",float(500)/500),
+	("花旗哩程","Qantas",float(500)/500),
+	("華南","Ponta",float(500)/500),
+	("華南","HappyGo",float(120)/1000),
+	("華南","OpenPoint",float(7500)/500),
+	("歐付寶","亞洲萬里通",float(100)/65),
+	("歐付寶","UUPoint",float(125)/45),
+	("歐付寶","Ponta",float(250)/10),	
+	("HamiPoint","UUPoint",float(20)/6), 
+	("HamiPoint","Ponta",float(750)/30),
+	("HamiPoint","LINE",float(25)/30),
+	("HamiPoint","華航",float(100)/63),
+	("HamiPoint","亞洲萬里通",float(100)/72),
+	("HamiPoint","OpenPoint",float(7600)/35),
+	("樂天","OpenPoint",float(90000)/300),
+	("樂天","UUPoint",float(1300)/500),
+	("亞洲萬里通","HamiPoint",float(700)/5000),
+	("亞洲萬里通","LINE",float(650)/5000),
+	("亞洲萬里通","PCHome",float(650)/5000),
+	("亞洲萬里通","樂天",float(700)/5000),
+	("亞洲萬里通","OpenPoint",float(168000)/5000),
+	("亞洲萬里通","UUPoint",float(2000)/5000),
+	("亞洲萬里通","HappyGo",float(1800)/5000),
+	("聯邦銀行","HamiPoint",float(50)/1000),
+	("聯邦銀行","長榮",float(1000)/10000),
+	("聯邦銀行","華航",float(1000)/10000),
+	("聯邦銀行","聯航",float(1000)/10000),
+	("中國信託","HamiPoint",float(80)/1000),
+	("中國信託","長榮",float(1)/6),
+	("中國信託","華航",float(1)/6),
+	("中國信託","亞洲萬里通",float(1)/3),
+	("中國信託","KrisFlyer",float(1)/6),
+	("中國信託","Ponta",float(1700)/1000),
+	("中國信託","UUPoint",float(250)/1000),
+	("中國信託","OpenPoint",float(11000)/500),	
+	("中國信託","HappyGo",float(80)/600),	
+	("LINE","HamiPoint",float(25)/30),
+	("LINE","HappyGo",float(50)/30),
+	("LINE","OpenPoint",float(8500)/30),
+	("LINE","Ponta",float(600)/30),
+	("LINE","UUPoint",float(125)/50),
+	("LINE","亞洲萬里通",float(100)/70),
+	("PCHome","HappyGo",float(50)/20),
+	("PCHome","亞洲萬里通",float(90)/60),
+	("PCHome","OpenPoint",float(12000)/40),
+	("PCHome","UUPoint",float(125)/45),
+	("長榮","HappyGo",float(360)/2000),
+	("長榮","UUPoint",float(500)/1500),
+	("長榮","Ponta",float(5000)/2000),
+	("彰銀","HappyGo",float(10)/100),
+	("彰銀","UUPoint",float(12)/100),
+	("台北富邦","HappyGo",float(100)/1000),	
+	("台北富邦","UUPoint",float(55)/500),	
+]
+
+G.add_weighted_edges_from(edges)
+
+cycles = list(nx.simple_cycles(G))
+
+# https://stackoverflow.com/questions/47341773/algorithm-to-multiply-edges-of-a-networkx-graph
+
+for cycle in cycles:
+	cycle.append(cycle[0])	
+
+for cycle in cycles:                       			# keep track of each path		    
+        pairs = zip(cycle, cycle[1:])                             # get sequence of nodes
+        product = 1                                       # reset product for this paths calculation
+        for pair in pairs:                                # for each pair of nodes in this path
+            an_edge = G.get_edge_data(pair[0], pair[1])   # get this edge's data
+            product *= an_edge['weight']                  # multiply all weights
+	if product > 0.8:		
+		print '{} has product = {}'.format(str(cycle).decode('string_escape'),product)
+
+# Build a graphviz file
+f = open("pic.dot", "w")
+f.write("digraph G {")
+for edge in edges:	
+	f.write('{}->{} [label="{}"]\n'.format(edge[0], edge[1], round(edge[2],2)))	
+f.write("}")
+f.close()
+
+# on Mac: brew install graphviz
+os.system("dot pic.dot -T png -o pic.png")
